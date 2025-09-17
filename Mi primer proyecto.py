28d71e636b492407b8a32057ea5ab27a9f2e3f11
jinhezhangxaloc <jinhe.zhang@campus.xaloc.org>
2025-09-17 09:16:52 +0200

    Add files via upload
    mi primer proyecto

---
+++ Mi primer proyecto.py
@@ -0,0 +1,71 @@
+# En este apartado estamos definiento variables
+var1="xaloc" # esta variable var1 es de tipo String y como puedes observar està entre comillas
+var2=4 # esta variable contienen un valor entero
+var3=5 
+var4=1.8 # esta variable contienen un valor de tipo float/decimal
+
+# este es un ejemplo de como no debo definir una variable var 3= "no es correcto"
+
+print("Hello my name is Jinhe") # ejemplo de la instrucción print que permite visualizar
+
+suma=var2+var3 
+print(suma) 
+print(var2+var3)
+print("El resultado de la suma de var2 + var3 es", suma)
+print(f"El resultado de la suma {var2} +{var3} es", suma)
+print("Error de instrucción")
+
+nombre=input("Introduce tu nombre")
+apellido1=input("Introduce tu primer apellido")
+apellido2=input("Introduce tu segundo apellido")
+nompre_completo= nombre + " " + apellido1 + " " + apellido2
+print(nompre_completo)
+
+a = 10
+b = 3
+
+print(a + b)  # Suma: 13
+print(a - b)  # Resta: 7
+print(a * b)  # Multiplicación: 30
+print(a / b)  # División: 3.333...
+print(a // b) # División entera: 3
+print(a % b)  # Módulo_divición: 1
+print(a ** b) # Exponenciación: 1000
+
+suma=var2+var3
+print(f"La suma de {var2} + {var3} es igual a", suma)
+
+resta=var2-var3
+print(f"La resta de {var2} - {var3} es igual a", resta)
+
+multiplicación=var2*var3
+print(f"La Multiplicación de {var2} * {var3} es igual a", multiplicación)
+
+división=var2/var3
+print(f"La división de {var2} / {var3} es igual a", división)
+
+división_entera=var2//var3
+print(f"La división_entera de {var2} // {var3} es igual a", división_entera)
+
+módulo_divición=var2%var3
+print(f"La módulo_divición de {var2} % {var3} es igual a", módulo_divición)
+
+exponenciació=var2**var3
+print(f"La exponenciació de {var2} ** {var3} es igual a", exponenciació)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
